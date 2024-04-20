da1326816699a443b9d001b57149da89fc8bf678
Casey Mulcahy <caseymulcahy@proton.me>
2024-04-20 09:53:45 -0400

    Solve part 3 in Rust

---
+++ rust/src/day3.rs
@@ -0,0 +1,54 @@
+fn common_item(xs: &[&str]) -> char {
+    return xs[0]
+        .chars()
+        .find(|c| xs[1..].iter().all(|s| s.contains(*c)))
+        .expect("should find common item");
+}
+
+fn common_rucksack_item(rucksack: &str) -> char {
+    let midpoint = rucksack.len() / 2;
+    return common_item(&[&rucksack[..midpoint], &rucksack[midpoint..]]);
+}
+
+fn priority(item: char) -> i64 {
+    return if item >= 'a' && item <= 'z' {
+        item as i64 - 'a' as i64 + 1
+    } else if item >= 'A' && item <= 'z' {
+        item as i64 - 'A' as i64 + 27
+    } else {
+        panic!("invalid item");
+    };
+}
+
+pub fn solve_part1(input: &str) -> i64 {
+    return input
+        .lines()
+        .map(|s| priority(common_rucksack_item(s)))
+        .sum();
+}
+
+pub fn solve_part2(input: &str) -> i64 {
+    let lines: Vec<&str> = input.lines().collect();
+    return (0..lines.len()/3).map(|i| priority(common_item(&lines[i*3..(i+1)*3]))).sum();
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::day3::{solve_part1, solve_part2};
+
+    fn example_input() -> String {
+        return fs::read_to_string("../examples/day03.txt").expect("example should be present");
+    }
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(solve_part1(&example_input()), 157);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(solve_part2(&example_input()), 70);
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,6 +4,7 @@ use std::fs;
 
 pub mod day1;
 pub mod day2;
+pub mod day3;
 
 fn main() {
     // parse CLI args
@@ -22,6 +23,7 @@ fn main() {
     let solutions: Vec<(fn(&str) -> i64, fn(&str) -> i64)> = vec![
         (day1::solve_part1, day1::solve_part2),
         (day2::solve_part1, day2::solve_part2),
+        (day3::solve_part1, day3::solve_part2),
     ];
 
     if let Some((part1, part2)) = solutions.get(day - 1) {
